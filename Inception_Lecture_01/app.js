092583a041faff491f92cc2835abe4786b87d7aa
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-07-08 15:41:32 +0530

    making food app inprogress

--- Inception_Lecture_01/app.js
+++ Inception_Lecture_01/app.js
@@ -1,51 +1,70 @@
-    //  <div id="parent">
-    //     <div id="child">
-    //         <h1>I am h1 tag</h1>
-    //         <h2>i am h2 tag</h2>
-    //     </div>
-    //     <div id="child2">
-    //         <h1>I am h1 tag</h1>
-    //         <h2>i am h2 tag</h2>
-    //     </div>
+ 
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 
-    // </div>
-    import React from 'react';
-    import ReactDOM from 'react-dom/client'
 
-    // const parent = React.createElement("div",
-    //     {id:"parent"},
-    //     [
-    //         React.createElement("div",{id:"child", key:'1'},
-    //             [
-    //                 React.createElement("h1",{id:"heading1" ,key:'1'},"I am h1 tag"),
-    //                 React.createElement("h2",{id:"heading2",key:'2'},"I am h2 tag"),
-    //             ]
-    //         ),
-    //          React.createElement("div",{id:"child2", key:'2'},
-    //         [
-    //             React.createElement("h1",{id:"heading3",key:'3'},"I am h1 tag"),
-    //             React.createElement("h2",{id:"heading4",key:'4'},"I am h2 tag"),
-    //         ]
-    //     )
+const Header =()=>{
+    return (
+        <div className='res-header'>
+           <div className='logo-container'>
+               <img className='logo' src="https://img.freepik.com/premium-vector/food-logo-design-with-leaf-creative-concept-premium-vector_526458-2675.jpg" alt="res-logo" />
 
-    //     ]
-    
-    // )
+           </div>
 
-    const Title = ()=>(
-        <h1>Hi Using function comonent </h1>
+           <div className='nav-items'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+           </div>
+        </div>
     )
+}
 
-    // element
+const ResCards =(props) =>{
+    return (
+        <div className='res-card'>
+            <img className='res-image' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/5f45f33f-b4c0-48b5-b8da-7309819958fe_361087.jpg" alt="image" />
 
-    const element  = 10000
+            <h2>{props.resName}</h2>
+            <h3>{props.cusine}</h3>
+            <div className='d-flex'>
+            <h4>4.4 stars</h4>
+            <h4>35 mins</h4>
+            </div>
 
-    const HeadingComponent = ()=>(
-        <><div id="Container">
-            My Name is Dheerendra {100+200}
-            {/* <h1> Main Heading : <Title/> </h1> */}
-        </div></>
-    );
+            <h5>Harsh Nagar</h5>
+        </div>
+    )
+}
+
+const Body =()=>{
+    return(
+        <div className='body'>
+           <div className='search'>Search</div>
+           <div className='res-container'>
+            {/* passing props to the component to make card dynmic  */}
+            <ResCards resName="The Good Bowl" cusine="Biryani,North Indians,Panjabi"></ResCards>
+            <ResCards resName="Kfc" cusine="Burger,Any one,Panjabi"></ResCards>
+
+          
+
+           </div>
+        </div>
+    )
+}
+
+const Applayout = () =>{
+ return (
+ <div className='app'>
+    <Header></Header>
+    <Body></Body>
+ </div>
+)
+}
+   
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent/>)
+root.render(<Applayout/>)
